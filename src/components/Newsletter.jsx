f4f399a4dcc4b460c14d78568aa9e6eb04f336f0
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-09-05 14:12:28 -0700

    completed project

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -4,13 +4,27 @@ import { useNavigate } from "react-router-dom";
 import illustration from "../assets/images/illustration-sign-up-desktop.svg";
 import { useState } from "react";
 
-const Newsletter = ({email, setEmail}) => {
-    const navigate = useNavigate()
+const Newsletter = ({ email, setEmail }) => {
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
 
-    const submtitHandler = (e) => {
-        e.preventDefault()
-        navigate('/success')
+  const changeHandler = (e) => {
+    setEmail(e.target.value);
+    if (error) {
+      setError(!error);
     }
+  };
+
+  const submtitHandler = (e) => {
+    e.preventDefault();
+    let re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email)) {
+      navigate("/success");
+    } else {
+      setError(!error);
+    }
+  };
 
   return (
     <div className="main-container">
@@ -18,25 +32,41 @@ const Newsletter = ({email, setEmail}) => {
         <h1 className="title">Stay Updated!</h1>
         <p>Join 60,000+ managers recieving monthly updates on:</p>
         <span className="bullet">
-            <img src={bullets} alt="check mark"/>
-            <p>Product discovery and bulding what matters</p>
+          <img src={bullets} alt="check mark" />
+          <p>Product discovery and bulding what matters</p>
         </span>
         <span className="bullet">
-            <img src={bullets} alt="check mark"/>
-            <p>Measuring to ensure updates are a success</p>
+          <img src={bullets} alt="check mark" />
+          <p>Measuring to ensure updates are a success</p>
         </span>
         <span className="bullet">
-            <img src={bullets} alt="check mark"/>
-            <p>And much more!</p>
+          <img src={bullets} alt="check mark" />
+          <p>And much more!</p>
         </span>
         <form onSubmit={submtitHandler} className="form">
-          <label htmlFor="email">Email Address</label>
-          <input type="email" placeholder="email@company.com" onChange={e=> setEmail(e.target.value)} value={email}/>
-          <button type="submit" className="btn">Subscribe to monthly newsletter</button>
+          <div className="msg-group">
+            <label htmlFor="email">Email Address</label>
+            <span className={!error ? "msg" : "error-msg"}>
+              Valid email required
+            </span>
+          </div>
+          <input
+            type="text"
+            placeholder="email@company.com"
+            onChange={changeHandler}
+            className={!error ? "email-input" : "email-input error"}
+          />
+          <button type="submit" className="btn">
+            Subscribe to monthly newsletter
+          </button>
         </form>
       </div>
       <div className="container img">
-        <img src={illustration} alt="business picture" className="illustration"/>
+        <img
+          src={illustration}
+          alt="business picture"
+          className="illustration"
+        />
       </div>
     </div>
   );
