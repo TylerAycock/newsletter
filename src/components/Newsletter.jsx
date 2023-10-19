d13e6588a0047b8dbf60e4bfb26c6337d3a096c3
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-10-19 13:24:14 -0700

    adjusted styling and added media queries

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,8 +1,8 @@
 import "./Newsletter.css";
-import bullets from "../assets/images/icon-list.svg";
 import { useNavigate } from "react-router-dom";
-import illustration from "../assets/images/illustration-sign-up-desktop.svg";
+import desktop from "../assets/images/illustration-sign-up-desktop.svg";
 import { useState } from "react";
+import mobile from "../assets/images/illustration-sign-up-mobile.svg";
 
 const Newsletter = ({ email, setEmail }) => {
   const navigate = useNavigate();
@@ -27,20 +27,50 @@ const Newsletter = ({ email, setEmail }) => {
   };
 
   return (
-    <div className="main-container">
-      <div className="container input">
-        <h1 className="title">Stay Updated!</h1>
+    <div className="main__container">
+      <div className="input__container">
+        <h1>Stay Updated!</h1>
         <p>Join 60,000+ managers recieving monthly updates on:</p>
         <span className="bullet">
-          <img src={bullets} alt="check mark" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21"
+          >
+            <g fill="none">
+              <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+              <path stroke="#FFF" strokeWidth="2" d="M6 11.381 8.735 14 15 8" />
+            </g>
+          </svg>
           <p>Product discovery and bulding what matters</p>
         </span>
         <span className="bullet">
-          <img src={bullets} alt="check mark" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21"
+          >
+            <g fill="none">
+              <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+              <path stroke="#FFF" strokeWidth="2" d="M6 11.381 8.735 14 15 8" />
+            </g>
+          </svg>
           <p>Measuring to ensure updates are a success</p>
         </span>
         <span className="bullet">
-          <img src={bullets} alt="check mark" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21"
+          >
+            <g fill="none">
+              <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+              <path stroke="#FFF" strokeWidth="2" d="M6 11.381 8.735 14 15 8" />
+            </g>
+          </svg>
           <p>And much more!</p>
         </span>
         <form onSubmit={submtitHandler} className="form">
@@ -61,13 +91,8 @@ const Newsletter = ({ email, setEmail }) => {
           </button>
         </form>
       </div>
-      <div className="container img">
-        <img
-          src={illustration}
-          alt="business picture"
-          className="illustration"
-        />
-      </div>
+      <img src={desktop} alt="business picture" className="desktop" />
+      <img src={mobile} className="mobile" />
     </div>
   );
 };
